0045eeac6c1df106fdddd964a114804e31399dca
Corentin BEAL <coco.bea@hotmail.fr>
2023-02-13 22:04:22 +0100

    Gestion du build

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,13 @@
-/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
+// rollup.config.js
+import merge from 'deepmerge';
+import { createSpaConfig } from '@open-wc/building-rollup';
 
-// Import rollup plugins
-import html from '@web/rollup-plugin-html';
-import {copy} from '@web/rollup-plugin-copy';
-import resolve from '@rollup/plugin-node-resolve';
-import {terser} from "rollup-plugin-terser";
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-import summary from 'rollup-plugin-summary';
+const baseConfig = createSpaConfig({
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: false
+});
 
-export default {
-  plugins: [
-    // Entry point for application build; can specify a glob to build multiple
-    // HTML files for non-SPA app
-    html({
-      input: 'index.html',
-    }),
-    // Resolve bare module specifiers to relative paths
-    resolve(),
-    // Minify HTML template literals
-    minifyHTML(),
-    // Minify JS
-    terser({
-      ecma: 2020,
-      module: true,
-      warnings: true,
-    }),
-    // Print bundle summary
-    summary(),
-    // Optional: copy any static assets to build directory
-    copy({
-      patterns: ['images/**/*'],
-    }),
-  ],
-  output: {
-    dir: 'build',
-  },
-  preserveEntrySignatures: 'strict',
-};
+export default merge(baseConfig, {
+  // any <script type="module"> inside will be bundled by rollup
+  input: './index.html'
+});
