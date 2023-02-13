d851784f224ed189452472bcf90210dcc5e49ca6
Corentin BEAL <coco.bea@hotmail.fr>
2023-02-13 21:02:26 +0100

    Début d'ajout des APIs

--- mood-element.js
+++ mood-element.js
@@ -7,7 +7,10 @@
 import {LitElement, html, css} from 'lit';
 
 export class MoodElement extends LitElement {
-  mood = '';
+  static properties = {
+    mood: {},
+    selected: {},
+  }
 
   static get styles() {
     return css`
@@ -15,26 +18,41 @@ export class MoodElement extends LitElement {
         display: flex;
         flex-direction: column;
         width: 228px;
-        align-items: center;
+        align-items: stretch;
+        margin: 24px 0 24px 0;
       }
 
-      p {
+      div.selected {
+        border: orange solid 12px;
+        margin: 12px 0 12px 0;
+      }
+
+      span {
         font-family: ParksFont;
         margin: 0;
         font-size: 3vh;
+        text-align: center;
+        background-color: #cf4f4f
       }
     `;
   }
 
+  getClass(selected) {
+    return selected ? 'selected' : '';
+  }
+
   constructor() {
     super();
+    this.mood = '';
+    this.selected = false;
   }
 
   render() {
+    console.log("Regénération enfant");
     return html`
-      <div>
+      <div class=${this.getClass(this.selected)}>
         <img src="./img/${this.mood.toLocaleLowerCase()}.png" />
-        <p>${this.mood}</p>
+        <span>${this.mood}</span>
       </div>
     `;
   }
--- mood-page-element.js
+++ mood-page-element.js
@@ -10,27 +10,67 @@ import './mood-element.js';
 
 export class MoodPageElement extends LitElement {
 
-  constructor() {
-    super();
-  }
+    static properties = {
+        items: {},
+    }
 
-  render() {
-    const itemTemplates = [];
-    const items = [
-        {text: 'Angry'},
-        {text: 'Girlboss'},
-        {text: 'Hungry'},
-        {text: 'Mad'},
-        {text: 'Tired'},
-    ]
+    static get styles() {
+        return css`
+          .wrapper {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            margin: 0 10% 0 10%;
+            justify-items: center;
+          }
+        `;
+    }
 
-    items.forEach(obj => itemTemplates.push(html`<mood-element .mood=${obj.text}></mood-element>`));
+    constructor() {
+        super();
+        this.items = []
 
-    return html`
-        <h1>MoodPageElement</h1>
-        ${itemTemplates}
-    `;
-  }
+        const request = new Request(
+            "http://localhost:3000/mood"
+        )
+        fetch(request)
+            .then((response) => response.json())
+            .then((array) => {
+                this.items = array.map(item => { return {mood: item} })
+            })
+
+        const requestUser = new Request(
+            "http://localhost:3000/mood/user"
+        )
+        fetch(requestUser)
+            .then((response) => response.json())
+            .then((obj) => {
+                console.log('request user', obj)
+            })
+    }
+
+    select(mood) {
+        this.items = this.items.map(obj => {
+            return {mood: obj.mood, selected: obj.mood === mood }
+        })
+        console.log("Sélection", this.items);
+    }
+
+    render() {
+        const itemTemplates = [];
+
+        this.items.forEach(obj => itemTemplates.push(
+            html`<mood-element .mood=${obj.mood} .selected=${!!obj.selected} @click=${() => this.select(obj.mood)}></mood-element>`
+        ));
+
+        console.log("Regénération");
+
+        return html`
+            <h1>MoodPageElement</h1>
+            <div class="wrapper">
+                ${itemTemplates}
+            </div>
+        `;
+    }
 }
 
 window.customElements.define('mood-page-element', MoodPageElement);
